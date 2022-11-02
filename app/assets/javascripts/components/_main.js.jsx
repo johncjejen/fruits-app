cb26a26b6bce546d3013a1f9646ea52e77fde002
johncjejen <john@john-VirtualBox>
2022-11-01 20:31:12 -0500

    Create react ruby

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,11 @@
+const Main = (props) => {
+    return(
+      <div className="main">
+        <div className="container">
+          <h1>Fruits are great!</h1>
+          <Body />
+        </div>
+      </div>
+    )
+}
+
